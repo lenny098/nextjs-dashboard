fb225fc66dadd9b41e189b632e7de8e5d62e88cb
lenny098 <32307017+lenny098@users.noreply.github.com>
2024-11-18 23:59:31 +0800

    Chapter 7: Fetching Data
    Changes:
    - Added content to Dashboard page
    - Refactored some provided codes

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -24,13 +24,12 @@ export const formatDateToLocal = (
 export const generateYAxis = (revenue: Revenue[]) => {
   // Calculate what labels we need to display on the y-axis
   // based on highest record and in 1000s
-  const yAxisLabels = [];
-  const highestRecord = Math.max(...revenue.map((month) => month.revenue));
+  const highestRecord = Math.max(...revenue.map(({ revenue }) => revenue));
   const topLabel = Math.ceil(highestRecord / 1000) * 1000;
 
-  for (let i = topLabel; i >= 0; i -= 1000) {
-    yAxisLabels.push(`$${i / 1000}K`);
-  }
+  const yAxisLabels = [...Array(topLabel / 1000 + 1).keys()] // Add zero
+    .reverse()
+    .map(key => `$${key}K`);
 
   return { yAxisLabels, topLabel };
 };
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import clsx from 'clsx';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -43,13 +44,15 @@ export function Card({
 
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-      <div className="flex p-4">
+      <div className="flex p-4 gap-2">
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+        <h3 className="text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
+        className={clsx(
+          lusitana.className,
+          'truncate rounded-xl bg-white px-4 py-8 text-center text-2xl'
+        )}
       >
         {value}
       </p>
